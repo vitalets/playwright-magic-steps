f45c6802edd7b761c02f282aa2239e5d558f5e94
Vitaliy Potapov <noginsk@rambler.ru>
2024-07-12 17:16:31 +0400

    add hooks and release script

---
+++ scripts/release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Exit on any error
+set -euo pipefail
+
+npm run lint
+npm run prettier
+npm ci
+npm test
+npm run example
+npm run build
+SKIP_GIT_HOOKS=1 npx np --yolo --no-release-draft --any-branch
