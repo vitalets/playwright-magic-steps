a6e1894a7f4edfd30a1682c7a6c3e5985515381f
Vitaliy Potapov <noginsk@rambler.ru>
2024-07-17 18:47:43 +0400

    release: skip build

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,5 +8,5 @@ npm run prettier
 npm ci
 npm run test:all
 npm run example
-npm run build
+# npm run build <- called inside npm run test:all
 SKIP_GIT_HOOKS=1 npx np --yolo --no-release-draft --any-branch
