ea13c043ce7c191498ac4a8b58eb981dc2325902
Vitaliy Potapov <noginsk@rambler.ru>
2024-10-22 18:48:51 +0400

    typo

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,14 +6,14 @@ set -euo pipefail
 rm -rf ./dist
 npx tsc -p tsconfig.build.json
 
-# make 'node_mosules/playwright-magic-steps' in 'test' dir:
+# make 'node_modules/playwright-magic-steps' in 'test' dir:
 # - Playwright does not transpile it b/c node_modules in path!
 rm -rf ./test/node_modules/playwright-magic-steps
 mkdir -p ./test/node_modules/playwright-magic-steps
 cp -R ./dist ./test/node_modules/playwright-magic-steps/
 cp ./package.json ./test/node_modules/playwright-magic-steps/package.json
 
-# make 'node_mosules/playwright-magic-steps' in 'example' dir:
+# make 'node_modules/playwright-magic-steps' in 'example' dir:
 rm -rf ./example/node_modules/playwright-magic-steps
 mkdir -p ./example/node_modules/playwright-magic-steps
 cp -R ./dist ./example/node_modules/playwright-magic-steps/
